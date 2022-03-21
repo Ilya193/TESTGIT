87350cb31e6c9962196b009e7b4df65934a50553
Ilya193 <ilya.komendantov@list.ru>
2022-03-21 22:42:24 +0300

    NAME3

--- app/src/main/java/com/xlwe/testgit/MainActivity.kt
+++ app/src/main/java/com/xlwe/testgit/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
 
     private val test2 = "SDFSDFSDFSDF"
 
+    private val test3 = "SDFSDFSDFSDF"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
